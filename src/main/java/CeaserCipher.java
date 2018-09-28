d90e8e7ea919fa25059de45908cd524c8870a543
fatahrez <fertahrez@gmail.com>
2018-09-28 11:53:26 +0300

    Add function ceaserCipher_takesStringInput_stringType in test/java/CeaserCipherTest that takes in a string as an input

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,3 +1,5 @@
 public class CeaserCipher {
+    public String doCeaserCipher (String wordForCeaserCipher){
 
+    }
 }
--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CeaserCipherTest {
+    @Test
+    public void ceaserCipher_takeStringInput_stringArray() {
+        CeaserCipher testCeaserCipher = new CeaserCipher();
+        String[] expectedOutput = {};
+        assertEquals(expectedOutput, testCeaserCipher.doCeaserCipher("hello"));
+    }
 }
