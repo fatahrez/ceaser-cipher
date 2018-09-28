ab758efcb3fa00e0458c0cb9fffbfbafdb7a5613
fatahrez <fertahrez@gmail.com>
2018-09-28 14:47:26 +0300

    Add test ceaserCipher_changesStringToArrayChar_arrayList in test/java/CeaserCipherTest

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -7,17 +7,17 @@ import static org.junit.Assert.*;
 
 public class CeaserCipherTest {
     @Test
-    public void ceaserCipher_takeStringInput_stringType() {
+    public void ceaserCipher_takeStringInput_arrayList() {
         CeaserCipher testCeaserCipher = new CeaserCipher();
         List<Object> expectedOutput = new ArrayList<Object>();
         expectedOutput.add("");
         assertEquals(expectedOutput, testCeaserCipher.doCeaserCipher("hello"));
     }
-//
-//    @Test
-//    public void ceaserCipher_changesStringTOArrayChar() {
-//        CeaserCipher testCeaserCipher = new CeaserCipher();
-//
-//    }
+
+    @Test
+    public void ceaserCipher_changesStringTOArrayChar() {
+        CeaserCipher testCeaserCipher = new CeaserCipher();
+        
+    }
 }
 
