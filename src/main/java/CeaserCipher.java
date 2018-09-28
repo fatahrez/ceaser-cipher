06252af896472f9d113ecb16191199bdd11c2df5
fatahrez <fertahrez@gmail.com>
2018-09-28 12:27:43 +0300

    Test for ceaserCipher_takesStringInput_stringType passes refractor and move on to next test

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,8 +1,6 @@
 public class CeaserCipher {
-    public boolean doCeaserCipher (String wordForCeaserCipher){
-        int wordConfirm = Integer.parseInt(wordForCeaserCipher);
-        Boolean test = wordForCeaserCipher.equals(wordConfirm);
-//        Integer changeIntTestToString = Integer.parseInt(test);
-        return test;
+    public String doCeaserCipher (String wordForCeaserCipher){
+        String wordTestForCeaserCipher = new String();
+        return  wordTestForCeaserCipher;
     }
 }
