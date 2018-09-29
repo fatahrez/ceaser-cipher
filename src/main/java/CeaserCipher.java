8b1b0b726d5b6997506e276265337da15d4e9102
fatahrez <fertahrez@gmail.com>
2018-09-29 15:35:27 +0300

    write code unDoCeaserCipher in src/main/java/CeaserCipher to decrypt input. for now its only the if for letters with capital is written

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -14,6 +14,14 @@ public class CeaserCipher {
         System.out.println(result);
         return result;
     }
-
+    public StringBuffer unDoCeaserCipher(String wordForCeaserCipher, int shiftIndex){
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < wordForCeaserCipher.length(); i--){
+            if (Character.isUpperCase(wordForCeaserCipher.charAt(i))){
+                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex - 65)% 26 + 65);
+                result.append(charr);
+            }
+            else
+        }
     }
 }
