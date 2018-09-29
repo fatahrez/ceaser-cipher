c27c8455663872860e59fa7a6bec01470fb3e68d
fatahrez <fertahrez@gmail.com>
2018-09-29 15:33:11 +0300

    write test ceaserCipher_doesDecryption_string() in src/test/java/CeaserCipherTest.java to test for encryption code

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -3,15 +3,17 @@ public class CeaserCipher {
         StringBuffer result = new StringBuffer();
         for (int i=0; i<wordForCeaserCipher.length(); i++){
             if (Character.isUpperCase(wordForCeaserCipher.charAt(i))) {
-                char ch = (char)(((int)wordForCeaserCipher.charAt(i) + shiftIndex - 65)% 26 + 65);
-                result.append(ch);
+                char charr = (char)(((int)wordForCeaserCipher.charAt(i) + shiftIndex - 65)% 26 + 65);
+                result.append(charr);
             }
             else {
-                char ch = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex -97)% 26 + 97);
-                result.append(ch);
+                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex -97)% 26 + 97);
+                result.append(charr);
             }
         }
         System.out.println(result);
         return result;
+    }
+
     }
 }
--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -15,11 +15,19 @@ public class CeaserCipherTest {
     }
 
     @Test
-    public void ceaserCipher_changesStringToArrayChar_charArray() {
+    public void ceaserCipher_doesEncryption_string() {
         CeaserCipher testCeaserCipher = new CeaserCipher();
         String expectedOutput = new String();
         expectedOutput = "lipps";
         assertEquals(expectedOutput, testCeaserCipher.doCeaserCipher("hello", 4));
 
+    }
+
+    @Test
+    public void ceaserCipher_doesDecryption_string() {
+        CeaserCipher testCeaaserCipher =  new CeaserCipher();
+        String expectedOutput = new String();
+        expectedOutput = "hello";
+        assertEquals(expectedOutput, testCeaaserCipher.unDoCeaserCipher("lipps"));
     }
 }
