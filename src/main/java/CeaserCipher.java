d961ce05e38b2bf1bf2afbf0eab1c6e636c55790
fatahrez <fertahrez@gmail.com>
2018-09-30 12:09:44 +0300

    write code to decrypt ceaserCipher in unDoCeaserCipher in src/main/java/CeaserCipher

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -15,18 +15,19 @@ public class CeaserCipher {
         return result;
     }
     public StringBuffer unDoCeaserCipher(String wordForCeaserCipher, int shiftIndex){
-        StringBuffer result = new StringBuffer();
-        for (int i = 0; i > wordForCeaserCipher.length(); i--){
-            if (Character.isUpperCase(wordForCeaserCipher.charAt(i))){
-                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex - 65)% 26 + 65);
-                result.append(charr);
-            }
-            else{
-                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex - 65)% 26 + 65);
-                result.append(charr);
-            }
-        }
-        System.out.println(result);
-        return result;
+//        StringBuffer result = new StringBuffer();
+//        for (int i = 0; i > wordForCeaserCipher.length(); i--){
+//            if (Character.isUpperCase(wordForCeaserCipher.charAt(i))){
+//                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex - 65)% 26 + 65);
+//                result.append(charr);
+//            }
+//            else{
+//                char charr = (char)(((int)wordForCeaserCipher.charAt(i)+ shiftIndex - 65)% 26 + 65);
+//                result.append(charr);
+//            }
+//        }
+//        System.out.println(result);
+//        return result;
+        return doCeaserCipher(wordForCeaserCipher, shiftIndex * -1);
     }
 }
