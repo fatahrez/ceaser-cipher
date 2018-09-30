b5c67aed9203026ee378bd4d7738ca41b7f23d48
fatahrez <fertahrez@gmail.com>
2018-09-30 14:45:59 +0300

    DRY code in src/main/java/CeaserCipher.java for doCeaserCipher()

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -11,7 +11,6 @@ public class CeaserCipher {
                 result.append(charr);
             }
         }
-        System.out.println(result);
         return result;
     }
     public StringBuffer unDoCeaserCipher(String wordForCeaserCipher, int shiftIndex){
